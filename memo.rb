2365d37b00f9cebb9d60d90f3c0eea18c9882a64
godlongooo <godlongooo@gmail.com>
2023-07-28 18:36:31 +0900

    再提出

--- memo.rb
+++ memo.rb
@@ -1,78 +1,51 @@
-# CSVファイルを扱うためのライブラリを読み込み
 require "csv"  
 
 puts "1 → 新規でメモを作成する / 2 → 既存のメモを編集する"
+memo_type = gets.to_i
 
-# ユーザーの入力値を取得し、数字へ変換
-memo_type = gets.to_i 
-# テキストを入れる箱
-memos = []
-# memos.csvが存在するなら参照してカウントアップする
-count = File.exist?('memos.csv') ? CSV.read('memos.csv').length : 0
-
-# memo_type」の値（1 or 2）によって処理を分岐
 # 新規メモ作成
 if memo_type == 1
-    #メモ作成
-    def make_memo(count)
-        puts"新規メモ作成"
-        #タイトル入力
-        puts"タイトルを入力してください"
-        title = gets.chomp
-        #テキスト入力
-        puts"本文を入力してください"
-        text = STDIN.read
-        #
-        count += 1
-        #配列取得
-        [count,title,text]
+
+    puts "新規でメモを作成します。拡張子を除いたファイル名を入力してください。(キャンセルする場合はendと入力してください)"
+    file_name = gets.chomp
+    # 入力キャンセル
+    if file_name == "end"
+        puts "入力をキャンセルし、プログラムを終了します。"
+        exit
     end
-    # title,textをmemoに取得
-    memo = make_memo(count)
-    # 新規メモ追加
-    memos.push(memo)
-    # CSVファイル作成 今回は新規メモを追加していくため"a"を設定
-    CSV.open('memos.csv','a') do |csv|
-        csv << memo
+    puts"メモの内容を記入して下さい。「Ctrl+D（あるいはCTRL+Z）」で保存します。"
+    text= STDIN.read
+
+    CSV.open("#{file_name}.csv" , "w") do |csv|  
+    csv.puts ["#{text}"]
     end
 
-    
-
-# 編集
+# メモ編集
 elsif memo_type == 2
-    # データ取得
-    def director(id)
-        data = CSV.read("memos.csv")
-        memo = data.find { |row| row[0].to_i == id } # IDに対応する行を検索する
-        memo
-    end
-    puts "編集したいメモのID番号を入力してください"
-    id = gets.chomp.to_i
-    memo = director(id)
-    p memo
 
-    # データ記入
-    puts"タイトルを入力してください"
-    title = gets.chomp
-    puts"本文を入力してください"
+    puts "既存のメモを編集します。拡張子を除いた既存ファイル名を入力してください。"
+    file_name = gets.chomp
+    # existでファイルが存在するか確認
+    if File.exist?("#{file_name}.csv")
+    # ファイルがあれば読み込みして表示
+    puts "既存のテキスト内容"
+    puts CSV.read("#{file_name}.csv")
+    else
+    # ファイルがなければプログラムを終了
+    puts "ファイルが存在しないため、プログラムを終了します。"
+    exit
+    end
+    
+    # 上書きしたcsvファイル作成
+    puts"編集内容を記入して下さい。(既存のテキストは削除されます)「Ctrl+D（あるいはCTRL+Z）」で上書き保存します。"
     text = STDIN.read
-    
-    # CSVデータをハッシュ形式で読み込む
-    data = CSV.read("memos.csv", headers: true)
-    data[id - 1]['title'] << title
-    data[id - 1]['text'] << text
-    
-    # CSVに編集データの挿入
-    # openにはデータ上書きのため"w"を設定
-    CSV.open("memos.csv", "w") do |csv|
-        csv << data.headers
-        data.each do |row|
-        csv << row
-        end
+    CSV.open("#{file_name}.csv" , "w") do |csv|
+        csv.puts ["#{text}"]
     end
 
+# 1,2以外が選択されたとき
 else
-    puts"1または2を入力してください"
+    puts "指定外の情報が入力されたためプログラムを終了します。1か2の数字を入力してください。"
 end
 
 
